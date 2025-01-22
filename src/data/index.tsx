d5ab5cc7214d935b56406185e89d83841cbab905
santanutextrickqqqqqqqqqqqqqqqqqqqqs <director@textricks.com>
2025-01-22 12:50:49 +0530

    Revert "updated raw code"
    This reverts commit 51de55e36e6e188b972dd0db6159a1a8662eb882.

--- src/data/index.tsx
+++
@@ -1,3 +0,0 @@
-export * from "./myProfile";
-export * from "./calls";
-export * from "./messages";
--- src/utils/stringUtils.ts
+++
@@ -1,6 +0,0 @@
-const getFirstLetter = (str: string): string => {
-  const trimmedString = str.trimStart(); // Removes any leading spaces
-  return trimmedString.charAt(0); // Returns the first character
-};
-
-export { getFirstLetter };
